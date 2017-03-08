56d94060ad14b9283807d3aa44b8c751827ebed1
Semapho <Semaphores@qq.com>
2017-03-09 01:02:20 +0800

    Test Commit.

--- app/src/main/java/com/semaphore/uav/uav_androidapp/MainActivity.java
+++ app/src/main/java/com/semaphore/uav/uav_androidapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.semaphore.uav.uav_androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.semaphore.uav.uav_androidapp.utillities.MyTestUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyTestUtil util = new MyTestUtil();
+
+        TextView tv = (TextView) findViewById(R.id.myTextView);
+        tv.setText(util.getValue());
+
     }
 }
---
+++ app/src/main/java/com/semaphore/uav/uav_androidapp/utillities/MyTestUtil.java
@@ -0,0 +1,11 @@
+package com.semaphore.uav.uav_androidapp.utillities;
+
+/**
+ * Created by Semaphore on 2017/3/9.
+ */
+
+public class MyTestUtil {
+    public String getValue() {
+        return "Hello from the utillity class";
+    }
+}
